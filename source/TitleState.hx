4fceb2ae3f637af702b59528a031769723677b5a
hayayayay <74699483+haya3218@users.noreply.github.com>
2021-08-02 16:03:40 +0800

    systools fix pt.3

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -476,6 +476,7 @@ class TitleState extends MusicBeatState
 
 	public static function restart()
 	{
+                #if SYSTOOLS
 		var os = Sys.systemName();
 		var args = "Test.hx";
 		var app = "";
@@ -500,5 +501,8 @@ class TitleState extends MusicBeatState
 		}
 		else
 			throw "Failed to restart bich";
+                #else
+                trace('absolutely nothing');
+                #end
 	}
 }
